c5efe0b1c3bc653420dc7402329270d349c27ba0
yuki0427 <88xx.manu.xx88@gmail.com>
2020-06-14 09:21:41 +0000

    top-page終了

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  root 'static_pages#top'
+  get 'sessions/new'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#top'
+  get '/signup', to: 'users#new'
+  
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
 end
